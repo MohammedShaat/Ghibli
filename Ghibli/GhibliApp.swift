82baaaec38f65d6bd6bd5ed989e204308d005707
Mohammed Shaat <mohammedshaat.it@gmail.com>
2026-07-18 08:24:03 -0400

    Initial Commit

---
+++ Ghibli/GhibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliApp.swift
+//  Ghibli
+//
+//  Created by Mohammed on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
